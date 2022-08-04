7b567f7e8170c61881a2416714009e0aca5afc40
DenisKurlykov <dkurlykov@gmail.com>
2022-08-04 22:28:20 +0400

    Переделана анимация

--- AnimationSpring/ViewControllers/ViewController.swift
+++ AnimationSpring/ViewControllers/ViewController.swift
@@ -18,6 +18,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        animationView.animate()
         descriptionLabel.text = animation.description
     }
     
